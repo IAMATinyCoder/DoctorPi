7cdb494164cc8cd29041f1b90822ed15e08d30e8
Christopher Kenrick <ckenrick@live.unc.edu>
2013-12-11 23:55:05 -0500

    Update DoctorPiAPI.py

--- programFiles/DoctorPiAPI.py
+++ programFiles/DoctorPiAPI.py
@@ -5,6 +5,8 @@ consumer_secret = "Input your consumer_secret here"
 access_token = "Input your access_token here"
 access_token_secret = "Input your access_token_secret here"
 
+# When you register your app with Twitter you will get the necessary credentials to replace the placeholders above
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -13,3 +15,5 @@ api = tweepy.API(auth)
 for page in tweepy.Cursor(api.home_timeline).pages(1):
         for item in page:
                 print item.text
+                
+# This command will display 1 page of your friend's latest tweets in the terminal
